66cf498441351c256524fc098ad92f33495e520e
zeli <akmanzuley90@gmail.com>
2023-11-15 19:59:30 +0300

    card design

--- NewsApp/app/src/main/java/com/example/newsapp/ui/fragment/SearchPageFragment.kt
+++ NewsApp/app/src/main/java/com/example/newsapp/ui/fragment/SearchPageFragment.kt
@@ -5,13 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.R
+import com.example.newsapp.databinding.ActivityMainBindingImpl
+import com.example.newsapp.databinding.FragmentSearchPageBinding
 
 class SearchPageFragment : Fragment() {
+    private lateinit var binding: FragmentSearchPageBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_page, container, false)
+        binding=FragmentSearchPageBinding.inflate(inflater, container, false)
+
+        binding.rv.layoutManager=LinearLayoutManager(requireContext())
+
+        // manuel birkaç veri
+
+
+
+
+
+
+        return binding.root
     }
 
 
