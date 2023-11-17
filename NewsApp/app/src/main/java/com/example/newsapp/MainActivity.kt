ddd8abefe6103bf06d431157f68bba8844fe6f01
Honor13 <onury.karaduman@gmail.com>
2023-11-17 18:39:00 +0300

    Profile Page OK

--- NewsApp/app/src/main/java/com/example/newsapp/MainActivity.kt
+++ NewsApp/app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -18,9 +18,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this@MainActivity,R.layout.activity_main)
 
-
-//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainNavHostFragment) as NavHostFragment
-//        val navController = navHostFragment.navController
-//        navController.navigate(R.id.loginScreenFragment)
     }
 }
--- NewsApp/app/src/main/java/com/example/newsapp/ui/fragment/ProfilePageFragment.kt
+++ NewsApp/app/src/main/java/com/example/newsapp/ui/fragment/ProfilePageFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import com.example.newsapp.R
@@ -13,11 +14,22 @@ import com.example.newsapp.databinding.FragmentProfilePageBinding
 
 class ProfilePageFragment : Fragment() {
 
-    private lateinit var binding:FragmentProfilePageBinding
+    private lateinit var binding : FragmentProfilePageBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(layoutInflater,R.layout.fragment_profile_page, container, false)
 
         binding.objectProfilePageFragment = this
+
+        binding.darkModeSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+
+            if (isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+            }else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+
+        }
 
         return binding.root
     }
@@ -28,6 +40,8 @@ class ProfilePageFragment : Fragment() {
         Navigation.findNavController(it).navigate(R.id.transactionGetInTouch)
 
     }
+
+
 
 
 }
